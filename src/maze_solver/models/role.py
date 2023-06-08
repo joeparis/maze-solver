cbf279d48bd52ab711e49e2aa6a2547b290f8200
Joe Paris <2662123+joeparis@users.noreply.github.com>
2023-06-08 11:16:50 -0700

    Define initial cell roles

---
+++ src/maze_solver/models/role.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from enum import IntEnum, auto
+
+
+class Role(IntEnum):
+    NONE = 0
+    ENEMY = auto()
+    ENTRANCE = auto()
+    EXIT = auto()
+    EXTERIOR = auto()
+    REWARD = auto()
+    WALL = auto()
