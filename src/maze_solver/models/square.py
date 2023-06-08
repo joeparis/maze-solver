515c2759c6265297ebdc4a9e88c375cf356e66cd
Joe Paris <2662123+joeparis@users.noreply.github.com>
2023-06-08 11:37:13 -0700

    Initial commit

---
+++ src/maze_solver/models/square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from dataclasses import dataclass
+
+from maze_solver.models.border import Border
+from maze_solver.models.role import Role
+
+
+@dataclass(frozen=True)
+class Square:
+    index: int
+    row: int
+    column: int
+    border: Border
+    role: Role = Role.NONE
